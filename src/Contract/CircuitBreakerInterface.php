9734405a1d6029e3371b49a93831f9dfbdf21c82
haaragard <haaragard@gmail.com>
2025-07-01 00:35:11 -0300

    add methods to CircuitBreakerInterface for state management

--- src/Contract/CircuitBreakerInterface.php
+++ src/Contract/CircuitBreakerInterface.php
@@ -6,5 +6,8 @@ namespace Haaragard\CircuitBreaker\Contract;
 
 interface CircuitBreakerInterface
 {
-
+    public function isOpen(string $key): bool;
+    public function recordFailure(string $key): void;
+    public function recordSuccess(string $key): void;
+    public function reset(string $key): void;
 }
