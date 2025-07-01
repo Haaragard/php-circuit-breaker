2df391360099731a9f0586f8cf087cf08f11dea9
haaragard <haaragard@gmail.com>
2025-07-01 00:35:30 -0300

    implement invoke method in CircuitBreakerFactory for service instantiation

--- src/Factory/CircuitBreakerFactory.php
+++ src/Factory/CircuitBreakerFactory.php
@@ -4,7 +4,50 @@ declare(strict_types=1);
 
 namespace Haaragard\CircuitBreaker\Factory;
 
+use Haaragard\CircuitBreaker\Adapter\LocalStorageAdapter;
+use Haaragard\CircuitBreaker\Config\Config;
+use Haaragard\CircuitBreaker\Contract\CircuitBreakerInterface;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Foundation\Application;
+use InvalidArgumentException;
+
 class CircuitBreakerFactory
 {
+    public function __construct(protected Application $app)
+    {
+    }
 
+    /**
+     * @throws InvalidArgumentException|BindingResolutionException
+     */
+    public function invoke(?string $service = null): CircuitBreakerInterface
+    {
+        if (is_null($service)) {
+            $service = config('circuit-breaker.service');
+        }
+
+        $serviceConfig = config("circuit-breaker.services.{$service}");
+        if (is_null($serviceConfig)) {
+            throw new InvalidArgumentException("Service configuration for '{$service}' not found.");
+        }
+
+        $serviceClass = $serviceConfig['service'] ?? $this->getDefaultLocalStorageAdapter();
+        if (! class_exists($serviceClass)) {
+            throw new InvalidArgumentException("Service class '{$serviceClass}' does not exist.");
+        }
+        if (! ($serviceClass instanceof CircuitBreakerInterface::class)) {
+            throw new InvalidArgumentException("Service class '{$serviceClass}' must implement " . CircuitBreakerInterface::class);
+        }
+
+        return $this->app->make($serviceClass, [
+            'config' => new Config(
+                enabled: config("circuit-breaker.enabled", false),
+            ),
+        ]);
+    }
+
+    private function getDefaultLocalStorageAdapter(): string
+    {
+        return LocalStorageAdapter::class;
+    }
 }
