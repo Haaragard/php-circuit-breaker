7183d90ffac5c085e682af588d5d9b5b104b4b34
haaragard <haaragard@gmail.com>
2025-07-01 00:35:34 -0300

    register CircuitBreakerInterface with CircuitBreakerFactory in ServiceProvider

--- src/Provider/ServiceProvider.php
+++ src/Provider/ServiceProvider.php
@@ -4,7 +4,37 @@ declare(strict_types=1);
 
 namespace Haaragard\CircuitBreaker\Provider;
 
-class ServiceProvider
-{
+use Haaragard\CircuitBreaker\Contract\CircuitBreakerInterface;
+use Haaragard\CircuitBreaker\Factory\CircuitBreakerFactory;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
+class ServiceProvider extends BaseServiceProvider implements DeferrableProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(
+            abstract: CircuitBreakerInterface::class,
+            concrete: CircuitBreakerFactory::class
+        );
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../../Config/circuit-breaker.php' => config_path('circuit-breaker.php'),
+        ], 'config');
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../Config/circuit-breaker.php',
+            'circuit-breaker'
+        );
+    }
+
+    public function provides(): array
+    {
+        return [
+            CircuitBreakerInterface::class => CircuitBreakerFactory::class,
+        ];
+    }
 }
