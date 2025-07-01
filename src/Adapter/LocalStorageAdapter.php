738c38cbdf1172b0ab032abc86e653accb77e2d7
haaragard <haaragard@gmail.com>
2025-06-30 21:27:32 -0300

    remove .idea

---
+++ src/Adapter/LocalStorageAdapter.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haaragard\CircuitBreaker\Adapter;
+
+use Haaragard\CircuitBreaker\Contract\CircuitBreakerInterface;
+
+class LocalStorageAdapter implements CircuitBreakerInterface
+{
+
+}
