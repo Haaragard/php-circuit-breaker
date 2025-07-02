536311bc3a3d9f267a4a3f42f9d2310376aad1ff
Diogo Antunes <15661230+Haaragard@users.noreply.github.com>
2025-07-02 20:42:37 -0300

    Tests: add feature test for service provider (#3)
    * feat: add ServiceProvider to testbench configuration
    
    * feat: add WithWorkbench trait to FeatureTestCase for enhanced testing capabilities
    
    * feat: add ServiceProviderTest for configuration and instance validation
    
    * refactor: remove unused WithWorkbench trait from ServiceProviderTest
    
    * fix: update composer.json to refine test namespace exclusion

--- tests/Feature/FeatureTestCase.php
+++ tests/Feature/FeatureTestCase.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Haaragard\Test\Feature;
 
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase;
 
 class FeatureTestCase extends TestCase
 {
+    use WithWorkbench;
 }
