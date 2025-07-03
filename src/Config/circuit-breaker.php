1a3c20f5c20a3dd901e2938eb69d4526ecbed960
haaragard <haaragard@gmail.com>
2025-07-03 00:47:42 -0300

    feat: add cache configuration for CircuitBreaker with CacheStorageAdapter

--- src/Config/circuit-breaker.php
+++ src/Config/circuit-breaker.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Haaragard\CircuitBreaker\Adapter\CacheStorageAdapter;
+
 return [
     'enabled' => true,
 
@@ -13,6 +15,15 @@ return [
             'timeout' => 1_000, // Timeout in milliseconds
             'failure_threshold' => 5, // Number of failures before circuit opens
             'reset_timeout' => 5_000, // Time in milliseconds before circuit resets
+        ], // Default service implementation for circuit breaker
+
+        'cache' => [
+            'timeout' => 1_000, // Timeout in milliseconds
+            'failure_threshold' => 5, // Number of failures before circuit opens
+            'reset_timeout' => 5_000, // Time in milliseconds before circuit resets
+            'key_prefix' => 'circuit-breaker:', // Prefix for cache keys
+            'cache_connection' => 'default', // Cache connection name
+            'service' => CacheStorageAdapter::class, // Default service name
         ], // Default service implementation for circuit breaker
 
 //        'new-service' => [
