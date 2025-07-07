eaf0de604c51ba9e35302b2cffdaac3b389c118f
haaragard <haaragard@gmail.com>
2025-07-07 20:49:15 -0300

    refactor: streamline service class resolution and configuration handling in CircuitBreakerFactory

--- src/Factory/CircuitBreakerFactory.php
+++ src/Factory/CircuitBreakerFactory.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Haaragard\CircuitBreaker\Factory;
 
-use Haaragard\CircuitBreaker\Adapter\CacheStorageAdapter;
-use Haaragard\CircuitBreaker\Adapter\LocalStorageAdapter;
-use Haaragard\CircuitBreaker\Config\CacheConfig;
-use Haaragard\CircuitBreaker\Config\Config;
-use Haaragard\CircuitBreaker\Contract\CacheConfigInterface;
 use Haaragard\CircuitBreaker\Contract\CircuitBreakerInterface;
-use Haaragard\CircuitBreaker\Contract\ConfigInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Foundation\Application;
 use InvalidArgumentException;
@@ -32,8 +26,8 @@ class CircuitBreakerFactory
             throw new InvalidArgumentException("Service configuration for '{$service}' not found.");
         }
 
-        $serviceClass = $serviceConfig['service'] ?? $this->resolveDefaultLocalStorageAdapter();
-        if (! class_exists($serviceClass)) {
+        $serviceClass = $serviceConfig['service'];
+        if (is_null($serviceClass) || ! class_exists($serviceClass)) {
             throw new InvalidArgumentException("Service class '{$serviceClass}' does not exist.");
         }
         if (! in_array(CircuitBreakerInterface::class, class_implements($serviceClass), true)) {
@@ -43,37 +37,11 @@ class CircuitBreakerFactory
         $isCircuitBreakerEnabled = config('circuit-breaker.enabled', false);
 
         return $this->app->make($serviceClass, [
-            'config' => $this->resolveConfig(
+            'config' => (new ConfigFactory)(
                 $serviceClass,
                 $isCircuitBreakerEnabled,
                 $serviceConfig
             ),
         ]);
-    }
-
-    private function resolveDefaultLocalStorageAdapter(): string
-    {
-        return LocalStorageAdapter::class;
-    }
-
-    private function resolveConfig(string $serviceClass, bool $isEnabled, array $config): ConfigInterface|CacheConfigInterface
-    {
-        return match ($serviceClass) {
-            LocalStorageAdapter::class => new Config(
-                enabled: $isEnabled,
-                timeout: $config['timeout'],
-                failureThreshold: $config['failure_threshold'],
-                resetTimeout: $config['reset_timeout'],
-            ),
-            CacheStorageAdapter::class => new CacheConfig(
-                enabled: $isEnabled,
-                timeout: $config['timeout'],
-                failureThreshold: $config['failure_threshold'],
-                resetTimeout: $config['reset_timeout'],
-                keyPrefix: $config['key_prefix'],
-                cacheConnection: $config['cache_connection'],
-            ),
-            default => throw new InvalidArgumentException("Unsupported service class '{$serviceClass}'."),
-        };
     }
 }
